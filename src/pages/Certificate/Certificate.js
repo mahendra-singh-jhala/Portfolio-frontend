fce307e405b0ce00ce339b60da952002cff45ef3
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-06-22 12:10:27 +0530

    add certificate

--- src/pages/Certificate/Certificate.js
+++ src/pages/Certificate/Certificate.js
@@ -1,17 +1,9 @@
-import React, { useState } from 'react'
-import AliceCarousel, { Link } from 'react-alice-carousel';
+import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { Link } from "react-router-dom"
 import { CertificateData } from './CertificateData';
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 const Certificate = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-
-    const slidePrev = () => setActiveIndex(activeIndex - 1)
-    const slideNext = () => setActiveIndex(activeIndex + 1)
-    
-    const synconSlideChange = ({ item }) => setActiveIndex(item);
-
     const items = CertificateData.map((item, index) => (
         <div key={index} className="flex flex-col items-center p-4">
             <img
@@ -21,8 +13,7 @@ const Certificate = () => {
             />
             <p className="mt-4 text-lg font-semibold text-center">{item.name}</p>
             <p className="text-sm text-gray-500 text-center mx-4">Issued by: {item.issue}</p>
-            <Link
-                to=""
+            <Link to={item.download} target="_blank"
                 className="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200"
             >
                 Download Certificate
@@ -32,35 +23,18 @@ const Certificate = () => {
 
     return (
         <div id="certificate">
-            <h1 className="mt-16 text-center text-4xl font-semibold">
-                Certificate
-            </h1>
-            <div className="mt-4">
+            <h1 className="mt-16 text-center text-4xl font-semibold">Certificate</h1>
+            <div className="relative p-5 mb-6 mt-4">
                 <AliceCarousel
                     items={items}
                     disableButtonsControls
-                    disableDotsControls
-                    onSlideChange={synconSlideChange}
-                    activeIndex={activeIndex}
+                    autoPlay
+                    autoPlayInterval={3000}
+                    infinite
                 />
-                {activeIndex !== items.length - 3 && items.length > 0 && (
-                    <button
-                        
-                        onClick={slideNext}
-                    >
-                        <FaAngleRight sx={{ transform: "rotate(90deg)", color: "black" }} />
-                    </button>
-                )}
-                {activeIndex !== 0 && items.length > 0 && (
-                    <button
-                        onClick={slidePrev}
-                    >
-                        <FaAngleLeft sx={{ transform: "rotate(90deg)", color: "black" }} />
-                    </button>
-                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Certificate
+export default Certificate;
--- src/pages/Certificate/CertificateData.js
+++ src/pages/Certificate/CertificateData.js
@@ -7,27 +7,36 @@ import frontendCertificate from "../../asset/Skyscanner.png"
 export const CertificateData = [
     {
         image: GuviCertificate,
-        name: "Certification in Full Stack Development(MERN) with AI Tools from IIT-M Pravartak, GUVI",
-        issue: "Guvi"
-    },
-    {
-        image: CCertificate,
-        name: "C Programming",
-        issue: "Udemy"
-    },
-    {
-        image: CPPCertificate,
-        name: "C++ Programming",
-        issue: "Udemy"
-    },
-    {
-        image: webDevlopmementCertificate,
-        name: "Web Development",
-        issue: "Udemy"
+        name: "Certification in Full Stack Development(MERN) with AI Tools",
+        issue: "IIT-M Pravartak, GUVI",
+        download: "https://drive.google.com/file/d/11Lu9uyAT5ahJ79nsFK8CJ8R2HqFgBc7D/view?usp=sharing"
     },
     {
         image: frontendCertificate,
         name: "Skyscanner - Front-End Software Engineering Job Simulation",
-        issue: "Forage"
+        issue: "Forage",
+        download: "https://drive.google.com/file/d/1TFyNB9FLiNoDoSsXMkhOqKjnVLoJsnua/view?usp=sharing"
+
+    },
+    {
+        image: CCertificate,
+        name: "C Programming",
+        issue: "Udemy",
+        download: "https://drive.google.com/file/d/1ckjz2iiV6e7cp_vT15LgeblKtCdOg7s_/view?usp=sharing"
+
+    },
+    {
+        image: CPPCertificate,
+        name: "C++ Programming",
+        issue: "Udemy",
+        download: "https://drive.google.com/file/d/1_OKJam08g7yfzn0-AXB-0J590PW9E3rV/view?usp=sharing"
+
+    },
+    {
+        image: webDevlopmementCertificate,
+        name: "Web Development",
+        issue: "Udemy",
+        download: "https://drive.google.com/file/d/1z6PAH9j-bYMha5nIztaaQsMB4p9Py_RQ/view?usp=sharing"
+
     }
 ]
